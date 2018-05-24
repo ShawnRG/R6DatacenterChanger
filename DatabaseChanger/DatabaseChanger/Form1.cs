651beb1a913545dd2b55dfb784b2eb492b260928
Shawn-Ross <gyina.shawn@gmail.com>
2018-05-25 00:14:03 +0200

    Working

--- DatabaseChanger/DatabaseChanger/Form1.cs
+++ DatabaseChanger/DatabaseChanger/Form1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,92 @@ namespace DatabaseChanger
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly string PATH = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"\\My Games\\Rainbow Six - Siege";
+        private readonly string BASE_URI = "https://r6db.com/api/v2/players/";
+
+        private readonly string[] hints = new[]
+            {"default", "eus", "cus", "scus", "wus", "sbr", "neu", "weu", "eas", "seas", "eau", "wja"};
+        
+        public Form1() 
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            foreach (string s in hints)
+            {
+                cboDatacenter.Items.Add(s);
+            }
+            
+            btnApply.Click += new EventHandler(this.Button_Pressed);
+            cboUUID.SelectedIndexChanged += new EventHandler(this.onIndexChanged);
+            var directories = Directory.GetDirectories(PATH);
+            
+            
+            getNames();
+            
+            //cboUUID.SelectedIndex = 0;
+            cboDatacenter.SelectedIndex = 0;
+            lblStatus.Text = "";
+            
+            //cboDatacenter.SelectedItem = getCurrentDatacenter();
+            
+           
+
         }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            var currDatacenter =
+                File
+                    .ReadLines(PATH + "\\" + cboUUID.SelectedItem + "\\GameSettings.ini")
+                    .First(s => s.Contains("DataCenterHint="));
+            
+            var newDatacenter =
+                File.ReadAllText(
+                    PATH + "\\" + cboUUID.SelectedItem + "\\GameSettings.ini");
+
+            newDatacenter = newDatacenter.Replace(currDatacenter, "DataCenterHint=" + cboDatacenter.SelectedItem);
+            
+            File.WriteAllText( PATH + "\\" + cboUUID.SelectedItem + "\\GameSettings.ini", newDatacenter);
+            
+            lblStatus.Text = "Succesfully applied";
+            
+        }
+
+        private string getCurrentDatacenter()
+        {
+            var currDatacenter =
+                File
+                    .ReadLines(PATH + "\\" + cboUUID.SelectedItem + "\\GameSettings.ini")
+                    .First(s => s.Contains("DataCenterHint=")).Replace("DataCenterHint=", "");
+
+            return currDatacenter;
+        }
+
+        private void onIndexChanged(object sender, EventArgs e)
+        {
+            cboDatacenter.SelectedItem = getCurrentDatacenter();
+        }
+
+        private async void getNames()
+        {
+            lblStatus.Text = "test";
+        }
+        
+        public string GetAsync(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            
+            using(HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        
     }
 }
--- DatabaseChanger/DatabaseChanger/Program.cs
+++ DatabaseChanger/DatabaseChanger/Program.cs
@@ -16,7 +16,9 @@ namespace DatabaseChanger
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            
             Application.Run(new Form1());
+            
         }
     }
 }
